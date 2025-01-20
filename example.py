04960bba02483cc134e0c63769f5cefc4b85cb5d
Hugh <hugh.evans-ext@gucci.com>
2025-01-20 15:18:39 +0000

    paginate large audio files

--- example.py
+++ example.py
@@ -1,5 +1,21 @@
 import whisper
+import os
+
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+
+os.system("python3 -m pip freeze | grep whisper")
+os.system("pip -V")
+
+myaudio = AudioSegment.from_file("harvard.wav" , "wav") 
+chunk_length_ms = 10000 # pydub calculates in millisec
+chunks = make_chunks(myaudio, chunk_length_ms) #Make chunks of 10 seconds
 
 model = whisper.load_model("turbo")
-result = model.transcribe("harvard.wav")
-print(result["text"])
+
+for i, chunk in enumerate(chunks):
+    chunk_name = "chunks/chunk{0}.wav".format(i)
+    print("exporting", chunk_name)
+    chunk.export(chunk_name, format="wav")
+    result = model.transcribe(chunk_name)
+    print(result["text"])
---
+++ example.sh
@@ -0,0 +1,2 @@
+# source /Users/hugh/turin_repos/whisper/.venv_v1/bin/activate
+time python3 example.py
